054d3626ca84f7fb03a45f58c4283f8b57b604e3
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-11-18 21:13:43 +0500

    Разработанный фрагмент навигационного меню. Дегтянников.

---
+++ app/src/main/java/com/example/recycler_view_degtiannikov/MenuNavigation.java
@@ -0,0 +1,16 @@
+package com.example.recycler_view_degtiannikov;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MenuNavigation extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+        return inflater.inflate(R.layout.fragment_menu_navigation,container,false);
+    }
+}
