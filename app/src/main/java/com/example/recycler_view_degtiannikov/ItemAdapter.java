5440f175fa43ceeb00c92348edd90a25c9087701
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-11-18 22:11:10 +0500

    Разработанная модель, контекст и адаптер данных для товаров.  Дегтянников.

---
+++ app/src/main/java/com/example/recycler_view_degtiannikov/ItemAdapter.java
@@ -0,0 +1,52 @@
+package com.example.recycler_view_degtiannikov;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+
+    private LayoutInflater Inflater;
+    private List<Item> Items;
+
+    ItemAdapter(Context context, List<Item> items) {
+        this.Inflater = LayoutInflater.from(context);
+        this.Items = items;
+    }
+
+    @Override
+    public ItemAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = Inflater.inflate(R.layout.item_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemAdapter.ViewHolder holder, int position) {
+        Item Item = Items.get(position);
+        holder.TvName.setText(Item.Name);
+        holder.TvModell.setText(Item.Modell);
+        holder.TvPrice.setText("₽" + String.valueOf(Item.Price));
+    }
+
+    @Override
+    public int getItemCount() {
+        return Items.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView TvName, TvModell, TvPrice;
+
+        ViewHolder(View view) {
+            super(view);
+            TvName = view.findViewById(R.id.tv_name);
+            TvModell = view.findViewById(R.id.tv_model);
+            TvPrice = view.findViewById(R.id.tv_price);
+        }
+    }
+}
